5b881556bc324476f3f4cd16fa99850d545c42f6
Marvin Eversley Silva <meverss@gmail.com>
2023-09-28 22:46:14 -0400

    Update

--- scripts/anim.js
+++ scripts/anim.js
@@ -1,35 +1,37 @@
-const notes = document.querySelectorAll('.box');
+const notes = document.querySelectorAll(".box");
 
-function triggerAnimation(entries){
-  entries.forEach(entry =>{
-    if(entry.isIntersecting){
-        entry.target.classList.add('show', 'animate__animated', 'animate__fadeIn');
-        obBoxes.unobserve(entry.target);
-        setTimeout(function(){entry.target.classList.remove('animate__animated', 'animate__fadeIn');}, 1500);
-    }
-  })  
+function triggerAnimation(entries) {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			entry.target.classList.remove("animate__animated", "animate__fadeOut", "hide");
+			entry.target.classList.add("animate__animated", "animate__fadeIn", "show");
+			// obBoxes.unobserve(entry.target);
+		} else {
+			entry.target.classList.remove("animate__animated", "animate__fadeIn", "show");
+			entry.target.classList.add("animate__animated", "animate__fadeOut", "hide");
+		}
+	});
 }
 
-function playVideo(entries){
-    if (entries[0].isIntersecting){
-        video.classList.add('animate__animated', 'animate__bounceIn');
-        video.play();
-        obVideo.unobserve(video);
-    } else {
-        video.classList.remove('animate__animated', 'animate__bounceIn');
-        video.pause();
-    }
-}
+// function playVideo(entries) {
+// 	if (entries[0].isIntersecting) {
+// 		video.classList.add("animate__animated", "animate__fadeIn");
+// 		video.play();
+// 		obVideo.unobserve(video);
+// 	} else {
+// 		video.classList.remove("animate__animated", "animate__fadeIn");
+// 		video.pause();
+// 	}
+// }
 
-const options={
-    root: null,
-    rootMargin: '-180px',
-    //threshold: .2
-}
+const options = {
+	root: null,
+	rootMargin: "-150px",
+	//threshold: .5
+};
 
 const obBoxes = new IntersectionObserver(triggerAnimation, options);
 
-notes.forEach(note =>{
-    obBoxes.observe(note);
-})
-
+notes.forEach((note) => {
+	obBoxes.observe(note);
+});
