b433b6afc7fddd600f0932d521a0da89d82706f3
Marvin Eversley Silva <meverss@gmail.com>
2023-10-14 16:07:21 -0400

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -794,5 +794,6 @@ if (location.href.includes('portfolio.html')) {
 fetch(`https://wwpspdb.kiniun.tech/portfolio/`)
 .then(data => data.json())
 .then(data => {
-	console.log(data[0].pictures);
+	const pictures = data.pictures;
+	console.log(pictures);
 })
