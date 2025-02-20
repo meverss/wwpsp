a2a4f52d81973a1f03fa73aacb6d313b309d64f4
Marvin Eversley Silva <meverss@gmail.com>
2025-02-20 02:09:05 -0500

    Testing media

--- server/database/db.js
+++ server/database/db.js
@@ -6,8 +6,8 @@ let dbServer
 
 export const connectDB = async ()=> {
   try {
-	await mongoose.connect('mongodb://localhost/wwpsp')
-//	await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.l4evt.mongodb.net/wwpsp`)
+//	await mongoose.connect('mongodb://localhost/wwpsp')
+	await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.l4evt.mongodb.net/wwpsp`)
 	console.log('>>> Connected to Database.')
   } catch (error) {
 	console.log(error)
