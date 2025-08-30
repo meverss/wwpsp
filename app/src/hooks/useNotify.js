93035301a8053978ce59baff8a8742ea763897b4
Marvin Eversley Silva <meverss@gmail.com>
2025-08-29 22:41:38 -0400

    Update notifications

--- app/src/hooks/useNotify.js
+++ app/src/hooks/useNotify.js
@@ -1,4 +1,5 @@
 import { useState, useCallback, useMemo } from 'react'
+import { nanoid } from 'nanoid'
 import { FaCircleCheck, FaTriangleExclamation } from "react-icons/fa6"
 import { IoInformationCircle } from "react-icons/io5"
 import { PiGearFill } from "react-icons/pi"
@@ -8,28 +9,32 @@ const NotificationItem = ({ id, type, title, message, duration, onClose })=> {
   let icon
   switch (type) {
     case "ok":
-      icon = <FaCircleCheck className="ntf_icon" style={{ fontSize:'32px', color: 'green' }} />
+      icon = <FaCircleCheck className="ntf_icon" style={{ fontSize:'30px', color: 'green' }} />
       break
     case "err":
-      icon = <FaTriangleExclamation className="ntf_icon" style={{ fontSize:'32px',color: 'red' }} />
+      icon = <FaTriangleExclamation className="ntf_icon" style={{ fontSize:'28px',color: 'red' }} />
       break
     case "inf":
-      icon = <IoInformationCircle className="ntf_icon" style={{ fontSize:'40px',color: 'yellow' }} />
+      icon = <IoInformationCircle className="ntf_icon" style={{ fontSize:'40px',color: 'yellow', left: '3px' }} />
       break
     case "sys":
-      icon =  <PiGearFill className="ntf_icon" style={{ fontSize:'32px',color: 'chocolate' }} />
+      icon =  <PiGearFill className="ntf_icon" style={{ fontSize:'30px',color: 'chocolate' }} />
       break
     default:
-      icon = <IoInformationCircle className="ntf_icon" style={{ fontSize:'40px',color: 'yellow' }} />
+      icon = <IoInformationCircle className="ntf_icon" style={{ fontSize:'40px',color: 'yellow', left: '3pc' }} />
   }
 
   return (
 	<>
     <div className="ntf_box animate-show" id={id} onClicks={onClose}>
       <div className="ntf_msg">
+      	<div className="ntf_bar">
+      	  {icon}
+      	  <h3 className="ntf_title">{title}</h3>
+      	  <p className="ntf_close" onClick={onClose}>x</p>
+        </div>
         <div className="ntf_text">
-          <h3>{icon}{title}<span onClick={onClose}>x</span></h3>
-          <p>{message}</p>
+          <p className="ntf_message">{message}</p>
         </div>
       </div>
     </div>
@@ -46,24 +51,17 @@ export const useNotify = ()=> {
   }, [])
 
   const showNotification = useCallback((type, message, options = {})=> {
-    const id = Date.now()
+    const id = nanoid()
     const title = options.title || 'Notification'
     const duration = options.duration || 5000
     
     setNotifications(prev => [
-      ...prev, 
-      { 
-        id, 
-        type, 
-        title, 
-        message,
-        duration
-      }
+      ...prev, { id, type, title, message, duration }
     ])
 
     // Auto-delete after 'duration'
     setTimeout(()=> {
-    //  removeNotification(id)
+      removeNotification(id)
     }, duration)
   }, [removeNotification])
   
